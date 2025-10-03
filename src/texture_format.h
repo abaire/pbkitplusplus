a7c119f85579e38103a3000f684b0bbf1d8d71a5
Erik Abair <erik.abair@gmail.com>
2025-10-03 09:21:43 -0700

    Adds method to map surface formats to texture formats.

--- src/texture_format.h
+++ src/texture_format.h
@@ -22,6 +22,15 @@ extern const int kNumFormats;
 
 const TextureFormatInfo &GetTextureFormatInfo(uint32_t nv_texture_format);
 
+//! Attempts to map the given surface format to a similar texture format.
+//!
+//! This is primarily useful when re-rendering a previously rendered surface as a texture.
+//!
+//! \param nv_surface_format NV097_SET_SURFACE_FORMAT_COLOR_* constant to map.
+//! \param use_linear Whether a linear or a swizzled texture format should be returned.
+//! \return NV097_SET_TEXTURE_FORMAT_COLOR_* mapping for the surface.
+uint32_t TextureFormatForSurfaceFormat(uint32_t nv_surface_format, bool use_linear = false);
+
 }  // namespace PBKitPlusPlus
 
 #endif  // PBKITPLUSPLUS_TEXTURE_FORMAT_H
